c18a7f793ff67ccf00d3ca3437fb24aecc6e6fa3
arrssenii <95125556+arrssenii@users.noreply.github.com>
2022-04-10 17:13:00 +0300

    Урок WEB. HTML Реклама с картинкой

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
-from flask import Flask
-from data import db_session
-from data.users import User
+from flask import Flask, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def main():
-    db_session.global_init("db/blogs.db")
-    # app.run()
+@app.route('/')
+def main_page():
+    return "Миссия Колонизация Марса"
 
 
-user = User()
-user.name = "Пользователь 1"
-user.about = "биография пользователя 1"
-user.email = "email@email.ru"
-db_sess = db_session.create_session()
-db_sess.add(user)
-db_sess.commit()
-user = User()
-user.name = "Пользователь 2"
-user.about = "биография пользователя 2"
-user.email = "email2@email.ru"
-db_sess = db_session.create_session()
-db_sess.add(user)
-db_sess.commit()
-user = User()
-user.name = "Пользователь 3"
-user.about = "биография пользователя 3"
-user.email = "email3@email.ru"
-db_sess = db_session.create_session()
-db_sess.add(user)
-db_sess.commit()
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promote():
+    promote_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                    'Мы сделаем обитаемыми безжизненные пока планеты.',
+                    'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(promote_list)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+                    <p>Вот она какая, красная планета.</p>
+                  </body>
+                </html>"""
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-primary" role="alert">
+                    Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                    Человечеству мала одна планета.
+                    </div>
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
